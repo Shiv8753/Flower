aad6c79cfde2720dab8566c3a8d0f350c9297a6b
Shiv8753 <pshiv6636@gmail.com>
2025-03-16 19:11:47 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U Devu  ').split('')
+    const titles = ('I LOVE U Devu😘❤️  ').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
