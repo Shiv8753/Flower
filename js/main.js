f7797f31ab3ad559ee9d4ccfeed57bbce2ae6305
Shiv8753 <pshiv6636@gmail.com>
2025-05-01 21:54:32 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U Sarah 🌹❤️ ').split('')
+    const titles = ('I LOVE U Harsha 🌹❤️ ').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
